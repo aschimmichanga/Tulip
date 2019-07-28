6fb3411be006bb182723c0fa91db0f5bd82a1129
aschimmichanga <aschimmichanga@users.noreply.github.com>
2019-07-28 04:16:24 -0400

    front end for sign in/sign up mostly done

--- app/src/main/java/com/aschimmichanga/tulip/screens/Welcome.java
+++ app/src/main/java/com/aschimmichanga/tulip/screens/Welcome.java
@@ -1,7 +1,10 @@
 package com.aschimmichanga.tulip.screens;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.aschimmichanga.tulip.R;
 
@@ -10,5 +13,19 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ConstraintLayout bg = findViewById(R.id.background);
+        bg.setBackgroundResource(R.drawable.default_gradient);
+    }
+
+    public void goToSignInScreen (View v)
+    {
+        Intent intent = new Intent (this, SignIn.class);
+        startActivity(intent);
+    }
+
+    public void goToMakeAccountScreen (View v)
+    {
+        Intent intent = new Intent (this, MakeAccount.class);
+        startActivity(intent);
     }
 }
